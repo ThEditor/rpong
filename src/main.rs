b3bd2ca5fdfbe9a9ae19cea6c74b0a76a9a5e6d1
theditor <theditormcpe@gmail.com>
2023-12-30 22:44:02 +0530

    fix: color mix

--- src/main.rs
+++ src/main.rs
@@ -14,47 +14,42 @@ fn main() {
     
     gl::load_with(|s| window.get_proc_address(s) as *const _);
 
+    let mut colors = [1.0, 1.0, 1.0, 1.0];
+
     while !window.should_close() {
+        unsafe {
+            gl::ClearColor(colors[0], colors[1], colors[2], colors[3]);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+        }
+
         window.swap_buffers();
 
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
-            handle_window_event(&mut window, event);
+            handle_window_event(&mut window, event, &mut colors);
         }
     }
 }
 
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
+fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent, colors: &mut [f32; 4]) {
     match event {
         glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
             window.set_should_close(true)
         }
         glfw::WindowEvent::Key(Key::W, _, Action::Press, _) => {
-            unsafe {
-                gl::ClearColor(1.0, 0.0, 0.0, 1.0);
-                gl::Clear(gl::COLOR_BUFFER_BIT);
-            }
+            *colors = [1.0, 0.0, 0.0, 1.0];
             println!("Red");
         }
         glfw::WindowEvent::Key(Key::A, _, Action::Press, _) => {
-            unsafe {
-                gl::ClearColor(0.0, 1.0, 0.0, 1.0);
-                gl::Clear(gl::COLOR_BUFFER_BIT);
-            }
+            *colors = [0.0, 1.0, 0.0, 1.0];
             println!("Green");
         }
         glfw::WindowEvent::Key(Key::S, _, Action::Press, _) => {
-            unsafe {
-                gl::ClearColor(0.0, 0.0, 1.0, 1.0);
-                gl::Clear(gl::COLOR_BUFFER_BIT);
-            }
+            *colors = [0.0, 0.0, 1.0, 1.0];
             println!("Blue");
         }
         glfw::WindowEvent::Key(Key::D, _, Action::Press, _) => {
-            unsafe {
-                gl::ClearColor(1.0, 1.0, 0.0, 1.0);
-                gl::Clear(gl::COLOR_BUFFER_BIT);
-            }
+            *colors = [1.0, 1.0, 0.0, 1.0];
             println!("Yellow");
         }
         _ => {}
