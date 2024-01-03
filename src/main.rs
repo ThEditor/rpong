8d02633843f83b613b846d2c82f136ec09da352d
theditor <theditormcpe@gmail.com>
2024-01-03 16:34:42 +0530

    chore(ex2): 2 triangles diff vao

--- src/main.rs
+++ src/main.rs
@@ -20,24 +20,24 @@ fn main() {
 
     let shader_program = ShaderProgram::new(VERTEX_SHADER_PATH, FRAGMENT_SHADER_PATH);
 
-    let vertices: [f32; 12] = [
-        0.5, 0.5, 0.0, 0.5, -0.5, 0.0, -0.5, -0.5, 0.0, -0.5, 0.5, 0.0,
+    let vertices1: [f32; 9] = [
+        0.5, 0.5, 0.0, 0.5, -0.5, 0.0, -0.5, -0.5, 0.0
     ];
 
-    let indices = [0, 1, 2, 2, 3, 0];
+    let indices1 = [0, 1, 2];
 
-    let vao = VertexArrayObject::new();
-    vao.bind();
+    let vao1 = VertexArrayObject::new();
+    vao1.bind();
 
-    let vbo = BufferObject::new(gl::ARRAY_BUFFER, gl::STATIC_DRAW);
-    vbo.bind();
-    vbo.store_f32_data(&vertices);
+    let vbo1 = BufferObject::new(gl::ARRAY_BUFFER, gl::STATIC_DRAW);
+    vbo1.bind();
+    vbo1.store_f32_data(&vertices1);
 
-    let ebo = BufferObject::new(gl::ELEMENT_ARRAY_BUFFER, gl::STATIC_DRAW);
-    ebo.bind();
-    ebo.store_i32_data(&indices);
+    let ebo1 = BufferObject::new(gl::ELEMENT_ARRAY_BUFFER, gl::STATIC_DRAW);
+    ebo1.bind();
+    ebo1.store_i32_data(&indices1);
 
-    let vap = VertexAttribePointer::new(
+    let vap1 = VertexAttribePointer::new(
         0,
         3,
         gl::FLOAT,
@@ -45,19 +45,51 @@ fn main() {
         3 * mem::size_of::<GLfloat>() as GLsizei,
         ptr::null(),
     );
-    vap.enable();
+    vap1.enable();
 
-    vbo.unbind();
-    vao.unbind();
+    vbo1.unbind();
+    vao1.unbind();
+
+    let vertices2: [f32; 9] = [
+        0.5, 0.5, 0.0, -0.5, -0.5, 0.0, -0.5, 0.5, 0.0,
+    ];
+
+    let indices2 = [0, 1, 2];
+
+    let vao2 = VertexArrayObject::new();
+    vao2.bind();
+
+    let vbo2 = BufferObject::new(gl::ARRAY_BUFFER, gl::STATIC_DRAW);
+    vbo2.bind();
+    vbo2.store_f32_data(&vertices2);
+
+    let ebo2 = BufferObject::new(gl::ELEMENT_ARRAY_BUFFER, gl::STATIC_DRAW);
+    ebo2.bind();
+    ebo2.store_i32_data(&indices2);
+
+    let vap2 = VertexAttribePointer::new(
+        0,
+        3,
+        gl::FLOAT,
+        gl::FALSE,
+        3 * mem::size_of::<GLfloat>() as GLsizei,
+        ptr::null(),
+    );
+    vap2.enable();
+
+    vbo2.unbind();
+    vao2.unbind();
 
     while !window.should_close() {
         Color::new(0.2, 0.3, 0.3, 1.0, gl::COLOR_BUFFER_BIT)
             .clear();
 
         shader_program.bind();
-        vao.bind();
-        unsafe { gl::DrawElements(gl::TRIANGLES, 6, gl::UNSIGNED_INT, std::ptr::null()) };
-        vao.unbind();
+        vao1.bind();
+        unsafe { gl::DrawElements(gl::TRIANGLES, 3, gl::UNSIGNED_INT, std::ptr::null()) };
+        vao2.bind();
+        unsafe { gl::DrawElements(gl::TRIANGLES, 3, gl::UNSIGNED_INT, std::ptr::null()) };
+        vao2.unbind();
 
         window.update();
     }
